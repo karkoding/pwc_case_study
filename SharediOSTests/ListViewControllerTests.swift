2fc1e09bbc27c0109bc1c604c3e298355f8fac59
Karthik K Manoj <karthik.manoj@ymedialabs.com>
2023-02-09 23:20:26 +0530

    test the number of sections count
    test the rendered section and rendered items

--- SharediOSTests/ListViewControllerTests.swift
+++ SharediOSTests/ListViewControllerTests.swift
@@ -20,39 +20,57 @@ final class ListViewControllerTests: XCTestCase {
         XCTAssertEqual(callCount, 1)
     }
     
-    func test_updateTableModel_doesNotRenderOnEmptyList() {
+    func test_updateTableModel_doesNotRenderListOnEmptyList() {
         let sut = ListViewController()
         
         sut.updateTableModel(sectionController: [])
         
-        XCTAssertEqual(sut.numberOfItemsRendered(), 0)
+        XCTAssertEqual(sut.numberOfSections(), 0)
     }
     
-    func test_updateTableModel_rendersOneItemOnListWithOneItem() {
+    func test_updateTableModel_doesNotRenderSectionOrItemsForAnItemWithNoSectionAndEmptyList() {
         let sut = ListViewController()
         let item1 = SectionController(headerController: nil, controllers: [])
         
         sut.updateTableModel(sectionController: [item1])
         
-        XCTAssertEqual(sut.numberOfItemsRendered(), 1)
+        XCTAssertEqual(sut.numberOfSections(), 1)
+        XCTAssertNil(sut.viewForHeaderIn(section: 0))
+        XCTAssertEqual(sut.numberOfItemsRenderedIn(section: 0), 0)
     }
     
-    func test_updateTableModel_rendersMultipleItemsOnListWithMultipleItems() {
+    func test_updateTableModel_rendersOneSectionWithOneItem() {
         let sut = ListViewController()
-        let item1 = SectionController(headerController: nil, controllers: [])
-        let item2 = SectionController(headerController: nil, controllers: [])
+        let item1 = SectionController(headerController: nil, controllers: [FakeCellController()])
         
-        sut.updateTableModel(sectionController: [item1, item2])
+        sut.updateTableModel(sectionController: [item1])
         
-        XCTAssertEqual(sut.numberOfItemsRendered(), 2)
+        XCTAssertEqual(sut.numberOfSections(), 1)
+        XCTAssertEqual(sut.numberOfItemsRenderedIn(section: 0), 1)
+    }
+    
+    class FakeCellController: NSObject, CellController {
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int { 1 }
+        
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell { UITableViewCell() }
     }
 
 }
 
 private extension ListViewController {
-    func numberOfItemsRendered() -> Int {
+    func numberOfSections() -> Int {
         let dataSource = tableView.dataSource!
         let itemsCount = dataSource.numberOfSections?(in: tableView)
         return itemsCount ?? .zero
+    }
+    
+    func numberOfItemsRenderedIn(section: Int) -> Int {
+        let dataSource = tableView.dataSource!
+        return dataSource.tableView(tableView, numberOfRowsInSection: section)
+    }
+    
+    func viewForHeaderIn(section: Int) -> UIView? {
+        let delegate = tableView.delegate
+        return delegate?.tableView?(tableView, viewForHeaderInSection: section)
     }
 }
