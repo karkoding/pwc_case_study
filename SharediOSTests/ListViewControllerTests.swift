98e7323baf20f35448dba72bfbcf504759751a99
Karthik K Manoj <karthik.manoj@ymedialabs.com>
2023-02-09 20:57:56 +0530

    rename to `ListViewControllerTests`

--- SharediOSTests/ListViewControllerTests.swift
+++ SharediOSTests/ListViewControllerTests.swift
@@ -1,6 +1,6 @@
 //
 //  ListiOSTests.swift
-//  ListiOSTests
+//  SharediOS
 //
 //  Created by Karthik K Manoj on 09/02/23.
 //
@@ -8,7 +8,7 @@
 import XCTest
 @testable import ListiOS
 
-final class ListiOSTests: XCTestCase {
+final class ListViewControllerTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
