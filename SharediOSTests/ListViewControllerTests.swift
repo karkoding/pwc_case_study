b3678ff793513c2dc57f1f90a380ae63dd4acc5f
Karthik K Manoj <karthik.manoj@ymedialabs.com>
2023-02-09 21:21:19 +0530

    test on calling viewDidLoad `onViewDidLoad` closure is invoked only once
    test updateTableModel renders empty views on empty list

--- SharediOSTests/ListViewControllerTests.swift
+++ SharediOSTests/ListViewControllerTests.swift
@@ -6,31 +6,34 @@
 //
 
 import XCTest
-@testable import ListiOS
+import SharediOS
 
 final class ListViewControllerTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func test_viewDidLoad_messagesOnViewDidLoadOnce() {
+        let sut = ListViewController()
+        
+        var callCount = 0
+        sut.onViewDidLoad = {  callCount += 1 }
+        
+        sut.loadViewIfNeeded()
+        
+        XCTAssertEqual(callCount, 1)
+    }
+    
+    func test_updateTableModel_rendersEmptyViewsOnLoadingEmptyList() {
+        let sut = ListViewController()
+        
+        sut.updateTableModel(sectionController: [])
+        
+        XCTAssertEqual(sut.numberOfItemsRendered(), 0)
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
+}
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+private extension ListViewController {
+    func numberOfItemsRendered() -> Int {
+        let dataSource = tableView.dataSource!
+        let itemsCount = dataSource.numberOfSections?(in: tableView)
+        return itemsCount ?? .zero
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
