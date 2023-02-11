57065dafaab2d216840cad68a2c612635dccac50
Karthik K Manoj <karthik.manoj@ymedialabs.com>
2023-02-11 13:54:56 +0530

    make use of makeSUT helper

--- SharediOSTests/NavigationControllerTests.swift
+++ SharediOSTests/NavigationControllerTests.swift
@@ -12,7 +12,7 @@ final class NavigationControllerTests: XCTestCase {
     func test_init_viewContollerList_deliversRootViewController() {
         let rootViewController = UIViewController()
         let navItem = NavigationController.RootNavigationItem(title: nil, leftButton: (nil, false), rightButton: (nil, false))
-        let sut = NavigationController(rootViewController: rootViewController, navItem: navItem)
+        let sut = makeSUT(rootViewController: rootViewController, navItem: navItem)
         
         XCTAssertEqual(sut.viewControllers, [rootViewController])
     }
@@ -20,7 +20,7 @@ final class NavigationControllerTests: XCTestCase {
     func test_init_topViewControllerTitle() {
         let title = "Home"
         let navItem = NavigationController.RootNavigationItem(title: title, leftButton: ("", true), rightButton: ("", true))
-        let sut = NavigationController(rootViewController: UIViewController(), navItem: navItem)
+        let sut = makeSUT(navItem: navItem)
         
         XCTAssertEqual(sut.rootNavigationItem?.title, title, "Expected title to be \(title)")
         
@@ -35,7 +35,7 @@ final class NavigationControllerTests: XCTestCase {
         let rightButton: (title: String, isEnabled: Bool) = ("Done", true)
         let navItem = NavigationController.RootNavigationItem(title: "", leftButton: leftButton, rightButton: rightButton)
         
-        let sut = NavigationController(rootViewController: UIViewController(), navItem: navItem)
+        let sut = makeSUT(navItem: navItem)
         
         XCTAssertEqual(sut.leftBarButtonItem?.title, leftButton.title, "Expected left button title to be \(leftButton.title)")
         XCTAssertEqual(sut.rightBarButtonItem?.title, rightButton.title, "Expected right button title to be \(rightButton.title)")
@@ -46,7 +46,7 @@ final class NavigationControllerTests: XCTestCase {
         let rightButton: (title: String, isEnabled: Bool) = ("", true)
         let navItem = NavigationController.RootNavigationItem(title: "", leftButton: leftButton, rightButton: rightButton)
         
-        let sut = NavigationController(rootViewController: UIViewController(), navItem: navItem)
+        let sut = makeSUT(navItem: navItem)
         
         XCTAssertEqual(sut.leftBarButtonItem?.isEnabled, leftButton.isEnabled, "Expected left button isEnabled to be \(leftButton.isEnabled)")
         XCTAssertEqual(sut.rightBarButtonItem?.isEnabled, rightButton.isEnabled, "Expected right button isEnabled to be \(rightButton.isEnabled)")
@@ -55,14 +55,14 @@ final class NavigationControllerTests: XCTestCase {
         let rightButton1: (title: String, isEnabled: Bool) = ("", false)
         let navItem1 = NavigationController.RootNavigationItem(title: "", leftButton: leftButton1, rightButton: rightButton1)
         
-        let sut1 = NavigationController(rootViewController: UIViewController(), navItem: navItem1)
+        let sut1 = makeSUT(navItem: navItem1)
         
         XCTAssertEqual(sut1.leftBarButtonItem?.isEnabled, leftButton1.isEnabled, "Expected left button isEnabled to be \(leftButton1.isEnabled)")
         XCTAssertEqual(sut1.rightBarButtonItem?.isEnabled, rightButton1.isEnabled, "Expected right button isEnabled to be \(rightButton1.isEnabled)")
     }
     
     private func makeSUT(
-        rootViewController: UIViewController,
+        rootViewController: UIViewController = UIViewController(),
         navItem: NavigationController.RootNavigationItem,
         file: StaticString = #filePath,
         line: UInt = #line
