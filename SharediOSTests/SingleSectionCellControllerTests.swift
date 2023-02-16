3961e2ec8b163c94ad98eca4ae7ec5d2c0cf2ac9
Karthik K Manoj <karthik.manoj@ymedialabs.com>
2023-02-16 23:37:21 +0530

    add empty implementation inside `SingleSectionCellControllerTests`

--- SharediOSTests/SingleSectionCellControllerTests.swift
+++ SharediOSTests/SingleSectionCellControllerTests.swift
@@ -5,4 +5,16 @@
 //  Created by Karthik K Manoj on 16/02/23.
 //
 
-import Foundation
+import XCTest
+import SharediOS
+
+final class SingleSectionCellControllerTests: XCTestCase {
+}
+
+// MARK: - Helpers
+extension SingleSectionCellControllerTests {
+    func makeSUT(file: StaticString = #filePath, line: UInt = #line) {
+       
+    }
+}
+
