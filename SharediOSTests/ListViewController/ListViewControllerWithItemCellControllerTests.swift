ebea108496af4c2cc35c2b6b19d2a74be5e4f6dd
Karthik K Manoj <karthik.manoj@ymedialabs.com>
2023-02-16 23:13:03 +0530

    add numberOfSection delegate method in `ItemCellController`

--- SharediOSTests/ListViewController/ListViewControllerWithItemCellControllerTests.swift
+++ SharediOSTests/ListViewController/ListViewControllerWithItemCellControllerTests.swift
@@ -96,6 +96,8 @@ extension ListViewControllerWithItemCellControllerTests {
             self.footerView = footerView
         }
         
+        func numberOfSections(in tableView: UITableView) -> Int { 1 }
+        
         func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int { 1 }
         
         func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell { itemCellView }
