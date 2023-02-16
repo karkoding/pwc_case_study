8f97bdc22c1e425179e13f11d15f7ea4615dacba
Karthik K Manoj <karthik.manoj@ymedialabs.com>
2023-02-16 23:11:17 +0530

    add shared helpers swift file and put common code for list view controller

---
+++ SharediOSTests/ListViewController/SharedHelpers.swift
@@ -0,0 +1,55 @@
+//
+//  SharedHelpers.swift
+//  SharediOSTests
+//
+//  Created by Karthik K Manoj on 16/02/23.
+//
+
+import UIKit
+import SharediOS
+
+public extension ListViewController {
+    var isSeparatorLineVisible: Bool {
+        !(tableView.separatorStyle == .none)
+    }
+    
+    var numberOfSections: Int {
+        tableView.numberOfSections
+    }
+    
+    var sectionHeaderTopPadding: CGFloat {
+        tableView.sectionHeaderTopPadding
+    }
+    
+    func headerView(for section: Int) -> UIView? {
+        let delegate = tableView.delegate
+        return delegate?.tableView?(tableView, viewForHeaderInSection: section)
+    }
+    
+    func footerView(for section: Int) -> UIView? {
+        let delegate = tableView.delegate
+        return delegate?.tableView?(tableView, viewForFooterInSection: section)
+    }
+    
+    func numberOfRenderedItemsIn(section: Int) -> Int {
+        tableView.numberOfRows(inSection: section)
+    }
+    
+    func itemAt(section: Int, row: Int) -> UITableViewCell? {
+        let dataSource = tableView.dataSource!
+        let indexPath = IndexPath(row: row, section: section)
+        return dataSource.tableView(tableView, cellForRowAt: indexPath)
+    }
+
+    func didSelectItemAt(section: Int, row: Int) {
+        let delegate = tableView.delegate
+        let indexPath =  IndexPath(row: row, section: section)
+        delegate?.tableView?(tableView, didSelectRowAt: indexPath)
+    }
+    
+    func didDeSelectItemAt(section: Int, row: Int) {
+        let delegate = tableView.delegate
+        let indexPath =  IndexPath(row: row, section: section)
+        delegate?.tableView?(tableView, didDeselectRowAt: indexPath)
+    }
+}
