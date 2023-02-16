ce7349874f705840c2c8faa229b254c401869c65
Karthik K Manoj <karthik.manoj@ymedialabs.com>
2023-02-16 23:07:16 +0530

    remove duplicate tests

--- SharediOSTests/ListViewControllerWithItemCellControllerTests.swift
+++ SharediOSTests/ListViewControllerWithItemCellControllerTests.swift
@@ -9,55 +9,6 @@ import XCTest
 import SharediOS
 
 final class ListViewControllerWithItemCellControllerTests: XCTestCase {
-    func test_viewDidLoad_messagesOnRequestToLoadOnce() {
-        let sut = makeSUT()
-        
-        var callCount = 0
-        sut.onRequestToLoad = { callCount += 1 }
-        
-        sut.loadViewIfNeeded()
-        
-        XCTAssertEqual(callCount, 1)
-    }
-    
-    func test_viewDidLoad_messagesConfigureListViewOnce() {
-        let sut = makeSUT()
-        
-        var callCount = 0
-        sut.configureListView = { _ in callCount += 1 }
-        
-        sut.loadViewIfNeeded()
-        
-        XCTAssertEqual(callCount, 1)
-    }
-    
-    func test_listViewDefaultProperties() {
-        let sut = makeSUT()
-        
-        XCTAssertEqual(sut.sectionHeaderTopPadding, 0, "Expected section header top padding to be 0 by default")
-        XCTAssertFalse(sut.isSeparatorLineVisible, "Expected no separator line by default")
-    }
-    
-    func test_configureListView_overridesDefaultProperties() {
-        let sut = makeSUT()
-        
-        sut.configureListView = {
-            $0.sectionHeaderTopPadding = 20
-            $0.separatorStyle = .singleLine
-        }
-        
-        XCTAssertEqual(sut.sectionHeaderTopPadding, 20, "Expected section header top padding to be 20")
-        XCTAssertTrue(sut.isSeparatorLineVisible, "Expected a separator line")
-    }
-    
-    func test_display_doesNotRenderOnEmptyList() {
-        let sut = makeSUT()
-        
-        sut.display(cellControllers: [])
-        
-        XCTAssertEqual(sut.numberOfSections, 0)
-    }
-    
     func test_display_rendersItemCellController() {
         let sut = makeSUT()
         let itemCellView = ItemCellControllerSpy.makeItemCellView()
