3189361d22516832a97bbe38869a462ac12ae170
Karthik K Manoj <karthik.manoj@ymedialabs.com>
2023-02-17 12:39:29 +0530

    move sectionCellController to sharedIOS module and update access specifiers to public

---
+++ SharediOS/Controllers/SectionCellController.swift
@@ -0,0 +1,38 @@
+//
+//  SectionCellController.swift
+//  SharediOS
+//
+//  Created by Karthik K Manoj on 17/02/23.
+//
+
+import UIKit
+
+public final class SectionCellController: NSObject {
+    private(set) var cellControllers: [CellController]
+    private(set) var headerView: UIView?
+    private(set) var footerView: UIView?
+    
+    init(cellControllers: [CellController], headerView: UIView?, footerView: UIView?) {
+        self.cellControllers = cellControllers
+        self.headerView = headerView
+        self.footerView = footerView
+    }
+}
+
+extension SectionCellController: CellController {
+    public func numberOfSections(in tableView: UITableView) -> Int { 1 }
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int { cellControllers.count }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        cellControllers[indexPath.item].tableView(tableView, cellForRowAt: indexPath)
+    }
+    
+    public func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        cellControllers[indexPath.item].tableView?(tableView, didSelectRowAt: indexPath)
+    }
+    
+    public func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        cellControllers[indexPath.item].tableView?(tableView, didDeselectRowAt: indexPath)
+    }
+}
--- SharediOSTests/SingleSectionCellControllerTests.swift
+++ SharediOSTests/SingleSectionCellControllerTests.swift
@@ -8,36 +8,6 @@
 import XCTest
 import SharediOS
 
-class SectionCellController: NSObject, CellController {
-    private(set) var cellControllers: [CellController]
-    private(set) var headerView: UIView?
-    private(set) var footerView: UIView?
-    
-    init(cellControllers: [CellController], headerView: UIView?, footerView: UIView?) {
-        self.cellControllers = cellControllers
-        self.headerView = headerView
-        self.footerView = footerView
-    }
-    
-    func numberOfSections(in tableView: UITableView) -> Int { 1 }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        cellControllers.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        cellControllers[indexPath.item].tableView(tableView, cellForRowAt: indexPath)
-    }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        cellControllers[indexPath.item].tableView?(tableView, didSelectRowAt: indexPath)
-    }
-    
-    func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
-        cellControllers[indexPath.item].tableView?(tableView, didDeselectRowAt: indexPath)
-    }
-}
-
 final class SingleSectionCellControllerTests: XCTestCase {
     func test_init_deliversEmptyCellController_onEmptyList() {
         let sut = makeSUT(cellControllers: [])
