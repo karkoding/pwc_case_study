1d08ec71cecee9f55be7a8db00bac44485155a77
Karthik K Manoj <karthik.manoj@ymedialabs.com>
2023-02-18 18:25:44 +0530

    move `SingleSelectionCellController` to production

--- SharediOSTests/ListViewController/SingleSelectionCellControllerTests.swift
+++ SharediOSTests/ListViewController/SingleSelectionCellControllerTests.swift
@@ -8,45 +8,6 @@
 import XCTest
 import SharediOS
 
-public final class SingleSelectionCellController: NSObject {
-    public private(set) var cellControllers: [CellController]
-    
-    public init(cellControllers: [CellController]) {
-        self.cellControllers = cellControllers
-    }
-}
-
-extension SingleSelectionCellController: CellController {
-    public func numberOfSections(in tableView: UITableView) -> Int {
-        cellControllers.reduce(0) { $0 + ($1.numberOfSections?(in: tableView) ?? 1) }
-    }
-    
-    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        cellController(for: section, tableView: tableView).tableView(tableView, numberOfRowsInSection: section)
-    }
-    
-    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        cellController(for: indexPath.section, tableView: tableView).tableView(tableView, cellForRowAt: indexPath)
-    }
-    
-    public func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        cellController(for: section, tableView: tableView).tableView?(tableView, viewForHeaderInSection: section)
-    }
-    
-    func cellController(for section: Int, tableView: UITableView) -> CellController {
-        var sectionCount = 0
-        for controller in cellControllers {
-            sectionCount += controller.numberOfSections?(in: tableView) ?? 1
-            if section < sectionCount {
-                return controller
-            }
-        }
-        
-        fatalError("Trying to access non existing cell controller for \(section)")
-    }
-}
-
-
 final class SingleSelectionCellControllerTests: XCTestCase {
     func test_init_deliversEmptyOnEmptyList() {
         let sut = makeSUT(cellControllers: [])
